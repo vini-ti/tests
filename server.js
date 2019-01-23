dbefbdc4b8568c3d7266f1fae2c9bc5a530fe985
vini-ti <46945330+vini-ti@users.noreply.github.com>
2019-01-23 00:44:16 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const http = require('http');
 var express = require('express');
 
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Ou beleza!</h1>');
+const port=process.env.PORT || 3000;
+
+app.set('port', port);
+app.use('/static', express.static(__dirname + '/static'));
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+app.get('/admin', function(request, response) {
+  response.sendFile(path.join(__dirname, 'admin.html'));
+});
+app.get('/painel', function(request, response) {
+  response.sendFile(path.join(__dirname, 'painel.html'));
+});
+// Starts the server.
+server.listen(port, function() {
+  console.log('Starting server on port '+ port);
 });
