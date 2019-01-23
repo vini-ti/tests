6e3590997bc8dfbf67b22cbcec4c262436271a19
vini-ti <46945330+vini-ti@users.noreply.github.com>
2019-01-23 00:23:43 -0200

    Update and rename app.js to server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
+var express = require('express');
+
 const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+res.end('<h1>Ou beleza!</h1>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
